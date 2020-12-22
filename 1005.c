9c2d26c0a936c1753a291870672147a096cc9de7
Jahid Hasan Sagor <56949779+JahidHasanSagor@users.noreply.github.com>
2020-12-22 22:59:03 +0600

    Add files via upload

---
+++ 1005.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    float A,B,C,MEDIA;
+    scanf("%f%f%f",&A,&B,&C);
+    MEDIA = ((2*A)+(3*B)+(5*C))/(2+3+5);
+    printf("MEDIA = %f\n",MEDIA);
+    return 0;
+
+}
